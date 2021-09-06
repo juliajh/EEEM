0645c311553498f3a159b123d60cf505e0ead515
parkmingue00 <88785472+parkmingue00@users.noreply.github.com>
2021-09-06 10:12:07 +0900

    models.py

--- gyeongsoton/main/models.py
+++ gyeongsoton/main/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+
+class newterm(models.Model):
+    question = models.CharField()
+    answer = models.CharField()
+    non_answer = models.CharField()
+
+
+class communityText(models.Model):
+    date = models.DateTimeField()
+    text = models.CharField(max_length=500)
+    like = models.IntegerField()
+    dis_like = models.IntegerField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE, default='')
+
+
+class communityComment(models.Model):
+    date = models.DateTimeField()
+    communitytext = models.ForeignKey(
+        communityText, on_delete=models.CASCADE, default='')
+    like = models.IntegerField()
+    dis_like = models.IntegerField()
+    text = models.CharField(max_length=300)
+    # 유저 fk 필요
+
+
+class manner(models.Model):
+    text = models.CharField(max_length=500)
+    like = models.IntegerField()
+    dis_like = models.IntegerField()
+    hashtag_me = models.CharField()
+    hashtag_you = models.CharField()
+    hashtag_situation = models.CharField()
+
+
+# 회원 클래스 생성 필요
