577b1270eabd9ff5fc5f518d915abedd1275592e
parkmingue00 <88785472+parkmingue00@users.noreply.github.com>
2021-10-16 17:55:31 +0900

    manner model text max_length

--- gyeongsoton/main/models.py
+++ gyeongsoton/main/models.py
@@ -9,13 +9,15 @@ class newterm(models.Model):
     answer = models.CharField(max_length=100)
     non_answer = models.CharField(max_length=100)
 
+
 class communityText(models.Model):
     date = models.DateTimeField()
     text = models.CharField(max_length=500)
     like = models.IntegerField()
     dis_like = models.IntegerField()
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default='')
-    title=models.CharField(max_length=30,default='')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE, default='')
+    title = models.CharField(max_length=30, default='')
 
     def __str__(self):
         return self.text[:50]+"..."
@@ -28,11 +30,12 @@ class communityComment(models.Model):
     like = models.IntegerField()
     dis_like = models.IntegerField()
     text = models.CharField(max_length=300)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default='')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE, default='')
 
 
 class manner(models.Model):
-    text = models.CharField(max_length=20)
+    text = models.CharField(max_length=18)
     like = models.IntegerField()
     dis_like = models.IntegerField()
     hashtag_me = models.CharField(max_length=20)
