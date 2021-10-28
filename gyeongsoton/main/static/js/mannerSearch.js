56e4aa6ae2b783f8590bca03b286638884ee38d1
parkmingue00 <parkm2ngyu00@naver.com>
2021-10-29 00:24:16 +0900

    manner search box

--- gyeongsoton/main/static/js/mannerSearch.js
+++
@@ -1,6 +0,0 @@
-const sideBar = document.querySelector('.side__bar');
-const findBtn = document.querySelector('.find__btn');
-
-findBtn.addEventListener('click', () => {
-  sideBar.classList.toggle('active');
-});
