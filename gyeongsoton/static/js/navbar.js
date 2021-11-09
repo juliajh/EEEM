b95f9d92b3c0161407ecc5ecd37024e5b76f0a69
juhee <juliajuhee222@gmail.com>
2021-11-10 05:10:20 +0900

    del static

--- gyeongsoton/static/js/navbar.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.querySelector('.navbar__toggleBtn');
-const navBarBtn = document.querySelector('.navbar__buttons');
-
-toggleBtn.addEventListener('click', () => {
-  navBarBtn.classList.toggle('active');
-});
